a827c762363c65b7e33193cff278be787ffdfd2d
jihunLim <limjihun204@naver.com>
2017-07-31 02:04:16 -0400

    complete door list

--- src/main/java/EvacGUI/EscapeDoor.java
+++ src/main/java/EvacGUI/EscapeDoor.java
@@ -2,7 +2,7 @@ package EvacGUI;
 
 public class EscapeDoor {
 	
-	float door_x, door_y;
+	float door_x, door_y, door_center_x, door_center_y;
 	int door_width, door_height;
 	
 	public EscapeDoor(float _door_x, float _door_y, int _door_width, int _door_height) {
@@ -11,5 +11,9 @@ public class EscapeDoor {
 		door_y = _door_y;
 		door_width = _door_width;
 		door_height = _door_height;
+		door_center_x = door_x + (door_width / 2);
+		door_center_y = door_y + (door_height / 2);
+		System.out.printf("x : %f, y: %f, w: %d, h: %d, cx: %f, cy: %f\n", door_x, door_y, door_width, door_height, 
+				door_center_x, door_center_y);
 	}
 }
