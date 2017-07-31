d3425c3c0bf193e361419e5c3b131307a6f75fbc
waynfashiom <wooseokjo@gmail.com>
2017-08-01 08:37:54 +0900

    wayn added task 3

---
+++ src/main/java/EvacGUI/state.java
@@ -0,0 +1,19 @@
+package EvacGUI;
+import EvacGUI.canvas;
+import EvacGUI.canvas.ball;
+
+public class state {
+	static int agent_number = 0;
+	
+	public float getCoordX(int i){
+		EvacGUI.canvas.ball temp_ball = EvacGUI.canvas.ballarray.get(i);
+		return temp_ball.x_pos;
+	}
+	
+	public float getCoordY(int i){
+		EvacGUI.canvas.ball temp_ball = EvacGUI.canvas.ballarray.get(i);
+		return temp_ball.y_pos;
+	}
+	
+	
+}
