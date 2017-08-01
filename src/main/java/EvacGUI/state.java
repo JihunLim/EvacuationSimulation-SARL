2881040a161be9b903f8f185806bea70212fedd3
waynfashiom <wooseokjo@gmail.com>
2017-08-01 12:15:50 +0900

    wayn 5

--- src/main/java/EvacGUI/state.java
+++ src/main/java/EvacGUI/state.java
@@ -17,11 +17,10 @@ public class state {
 	*/
 	
 
-    public void change_direction(int agent_id,int closestdoor_x, int closestdoor_y){
-    		synchronized (this) {
+    public synchronized static void change_direction(int agent_id,int closestdoor_x, int closestdoor_y){
 			float x=EvacGUI.canvas.ballarray.get(agent_id).x_pos;
     	float y=EvacGUI.canvas.ballarray.get(agent_id).y_pos;
-    		
+    	System.out.println("change direction for " + agent_id);
     	float diff_x = closestdoor_x-x;
     	float diff_y = closestdoor_y-y;
     		
@@ -36,11 +35,10 @@ public class state {
     		diff_x=-1;
     	}
     	
-    	EvacGUI.canvas.ballarray.get(0).dir_x=diff_x;
-    	EvacGUI.canvas.ballarray.get(0).dir_y=normalized_diff_y;
+    	EvacGUI.canvas.ballarray.get(agent_id).dir_x=diff_x;
+    	EvacGUI.canvas.ballarray.get(agent_id).dir_y=normalized_diff_y;
     	System.out.println(diff_x);
     	System.out.println(normalized_diff_y);
-    	
-    }
+    
 	}
 }
